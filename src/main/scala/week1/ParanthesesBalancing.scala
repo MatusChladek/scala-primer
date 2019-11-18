b4745288084457a7e886afa8d8f867eaa62a6851
Matúš Chládek <mmchladek@gmail.com>
2019-11-18 15:09:13 +0100

    paranthese to tailrec

--- src/main/scala/week1/ParanthesesBalancing.scala
+++ src/main/scala/week1/ParanthesesBalancing.scala
@@ -1,30 +1,27 @@
 package week1
 
+import scala.annotation.tailrec
+
 object ParanthesesBalancing extends App {
   def balance(chars: List[Char]): Boolean = {
+    @tailrec
     def func(chars: List[Char], count_open_parantheses: Int): Boolean = {
       if (chars.isEmpty) {
         count_open_parantheses == 0
+      } else if (count_open_parantheses < 0) {
+        false
       } else {
         val head = chars.head
-        val remainder =
-          if (head.equals('(')) {
-            count_open_parantheses + 1
-          } else if (head.equals(')')) {
-            count_open_parantheses - 1
-          } else {
-            count_open_parantheses
-          }
-        if (remainder >= 0) {
-          func(chars.tail, remainder)
+        if (head.equals('(')) {
+          func(chars.tail, count_open_parantheses + 1)
+        } else if (head.equals(')')) {
+          func(chars.tail, count_open_parantheses - 1)
         } else {
-          false
+          func(chars.tail, count_open_parantheses)
         }
       }
     }
 
     func(chars, 0)
   }
-
-  print(balance("()".toList))
 }
