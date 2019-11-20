6adb0042dd292f9f3355c66eccf520c33c10c514
Matúš Chládek <mmchladek@gmail.com>
2019-11-20 14:15:29 +0100

    tests representation

--- src/main/scala/week2/Representation.scala
+++ src/main/scala/week2/Representation.scala
@@ -1,6 +1,6 @@
 package week2
 
-object Representation extends App {
+object Representation {
   type FunSet = Int => Boolean
 
   /**
@@ -73,16 +73,4 @@ object Representation extends App {
   def printSet(s: FunSet) {
     println(toString(s))
   }
-
-  println(contains(singletonSet(1), 1))
-
-  val s1 = singletonSet(4)
-  val s2 = singletonSet(8)
-  val s3 = singletonSet(3)
-
-  val y = union(s1, s2)
-  val z = union(y, s3)
-  printSet(y)
-  printSet(s3)
-  printSet(map(z, x => x * x))
 }
---
+++ src/test/scala/week2/RepresentationTest.scala
@@ -0,0 +1,87 @@
+package week2
+
+import org.scalatest.FunSuite
+
+class RepresentationTest extends FunSuite {
+  /**
+   * Link to the scaladoc - very clear and detailed tutorial of FunSuite
+   *
+   * http://doc.scalatest.org/1.8/index.html#org.scalatest.FunSuite
+   *
+   * Operators
+   *  - test
+   *  - ignore
+   *  - pending
+   */
+
+  test("string take") {
+    val message = "hello, world"
+    assert(message.take(5) == "hello")
+  }
+
+  /**
+   * For ScalaTest tests, there exists a special equality operator "===" that
+   * can be used inside "assert". If the assertion fails, the two values will
+   * be printed in the error message.
+   */
+
+  import week2.Representation._
+
+  test("contains is implemented") {
+    assert(contains(x => true, 100))
+  }
+
+  /**
+   * When writing tests, one would often like to re-use certain values for multiple
+   * tests. For instance, we would like to create an Int-set and have multiple test
+   * about it.
+   *
+   * Instead of copy-pasting the code for creating the set into every test, we can
+   * store it in the test class using a val:
+   *
+   * val s1 = singletonSet(1)
+   *
+   * However, what happens if the method "singletonSet" has a bug and crashes? Then
+   * the test methods are not even executed, because creating an instance of the
+   * test class fails!
+   *
+   * Therefore, we put the shared values into a separate trait (traits are like
+   * abstract classes), and create an instance inside each test method.
+   *
+   */
+
+  trait TestSets {
+    val s1 = singletonSet(1)
+    val s2 = singletonSet(2)
+    val s3 = singletonSet(3)
+  }
+
+  test("singletonSet(1) contains 1") {
+    new TestSets {
+      assert(contains(s1, 1), "Singleton")
+    }
+  }
+
+  test("union contains all elements") {
+    new TestSets {
+      val s = union(s1, s2)
+      assert(contains(s, 1), "Union 1")
+      assert(contains(s, 2), "Union 2")
+      assert(!contains(s, 3), "Union 3")
+    }
+  }
+
+  test("forall method") {
+    new TestSets {
+      val x = union(s1, s2)
+      assert(forall(x, s => s < 3), "testing with int 1")
+    }
+  }
+
+  test("map method") {
+    new TestSets {
+      val s = map(s2, x => x * x)
+      assert(contains(s, 4), "Map power of 2")
+    }
+  }
+}
