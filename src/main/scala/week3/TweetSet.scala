1a85fc9f74e909b419246686d0c19d1c424445f3
Matúš Chládek <mmchladek@gmail.com>
2019-11-22 13:11:49 +0100

    working filter on tweets

---
+++ src/main/scala/week3/TweetSet.scala
@@ -0,0 +1,101 @@
+package week3
+
+object TweetSet {
+
+  class Tweet(val text: String, val retweets: Int) {
+
+    override def toString: String = this.text
+
+  }
+
+  abstract class TweetSet {
+    def include(x: Tweet): TweetSet
+
+    def contains(x: Tweet): Boolean
+
+    def isEmpty: Boolean
+
+    /** This method takes a predicate and returns a subset of all the elements
+     * in the original set for which the predicate is true.
+     */
+    def filter(p: Tweet => Boolean): TweetSet
+
+    /**
+     * Takes an accumulator set as a second argument.
+     * This accumulator contains the ongoing result of the filtering.
+     */
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet
+
+    def head: Tweet
+  }
+
+  object Empty extends TweetSet {
+    def include(x: Tweet): TweetSet = new NonEmpty(x, Empty, Empty)
+
+    def contains(x: Tweet): Boolean = false
+
+    def isEmpty: Boolean = true
+
+    def filter(p: Tweet => Boolean): TweetSet = this
+
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
+
+    def head: Tweet = throw new NoSuchElementException("Empty.head")
+
+    override def toString: String = "."
+  }
+
+  /**
+   * The tweets are indexed by their text bodies.
+   * The bodies of all tweets on the left are lexicographically smaller
+   * than elem and all bodies of elements on the right are lexicographically greater.
+   */
+  class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
+
+    def include(x: Tweet): TweetSet = {
+      if (x.text < elem.text) {
+        new NonEmpty(elem, left.include(x), right)
+      } else if (x.text > elem.text) {
+        new NonEmpty(elem, left, right.include(x))
+      } else this
+    }
+
+    def contains(x: Tweet): Boolean = {
+      if (x.text < elem.text) {
+        this.left.contains(x)
+      } else if (x.text > elem.text) {
+        this.right.contains(x)
+      } else true
+    }
+
+    def isEmpty: Boolean = false
+
+    def filter(p: Tweet => Boolean): TweetSet = this.filterAcc(p, Empty)
+
+    def head: Tweet = elem
+
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+//      println(p(this.head))
+//      var updated_acc: TweetSet = Empty
+
+      val updated_acc = if (p(this.head)) acc.include(this.head) else acc
+//      println(updated_acc)
+//      if (p(this.head)) {
+//        updated_acc = acc.include(this.head)
+//        println("current1" + updated_acc)
+//      } else {
+//        updated_acc = acc
+//        println("current2" + updated_acc)
+//      }
+//      println("current up 1" + updated_acc)
+      val leftAcc = left.filterAcc(p, updated_acc)
+//      println("current up 2" + leftAcc)
+      right.filterAcc(p, leftAcc)
+    }
+
+
+    override def toString: String = "{" + left + elem + right + "}"
+
+  }
+
+}
---
+++ src/main/scala/week3/TweetSetMain.scala
@@ -0,0 +1,14 @@
+package week3
+
+import week3.TweetSet._
+
+object TweetSetMain extends App {
+  val leaf1 = new NonEmpty(new Tweet("subTweet3", 3), Empty, Empty)
+  val leaf2 = new NonEmpty(new Tweet("axubTweet3", 4), Empty, Empty)
+
+  val t1 = new NonEmpty(new Tweet("subTweet13", 3), Empty, leaf1)
+  val t2 = new NonEmpty(new Tweet("xubTweet2", 4), leaf2, Empty)
+  val t = new NonEmpty(new Tweet("theTweet2", 2), t1, t2)
+  println(t)
+  println(t.filter(x => x.text.contains("3")))
+}
