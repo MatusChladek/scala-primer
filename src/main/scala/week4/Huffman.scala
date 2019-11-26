c2a8e737c24fa6de76f04c3601ec5c21c5183715
Matúš Chládek <mmchladek@gmail.com>
2019-11-26 15:38:10 +0100

    finish

--- src/main/scala/week4/Huffman.scala
+++ src/main/scala/week4/Huffman.scala
@@ -102,7 +102,7 @@ trait Huffman extends HuffmanInterface {
   /**
    * Checks whether the list `trees` contains only one single code tree.
    */
-  def singleton(trees: List[CodeTree]): Boolean = ???
+  def singleton(trees: List[CodeTree]): Boolean = if (trees.length == 1) true else false
 
   /**
    * The parameter `trees` of this function is a list of code trees ordered
@@ -116,7 +116,24 @@ trait Huffman extends HuffmanInterface {
    * If `trees` is a list of less than two elements, that list should be returned
    * unchanged.
    */
-  def combine(trees: List[CodeTree]): List[CodeTree] = ???
+  def combine(trees: List[CodeTree]): List[CodeTree] = {
+    def combineAcc(forkNode: Fork, bigTrees: List[CodeTree]): List[CodeTree] = {
+      if (weight(forkNode) < weight(bigTrees.head)) {
+        val newTree = forkNode :: bigTrees
+        newTree
+      } else {
+        val newTree = bigTrees.head :: combineAcc(forkNode, bigTrees.tail)
+        newTree
+      }
+    }
+
+    if (trees.length < 3) trees
+    else {
+      val forkNode = makeCodeTree(trees.head, trees.tail.head)
+      val newTrees = trees.drop(2)
+      combineAcc(forkNode, newTrees)
+    }
+  }
 
   /**
    * This function will be called in the following way:
@@ -129,7 +146,12 @@ trait Huffman extends HuffmanInterface {
    * In such an invocation, `until` should call the two functions until the list of
    * code trees contains only one single tree, and then return that singleton list.
    */
-  def until(done: List[CodeTree] => Boolean, merge: List[CodeTree] => List[CodeTree])(trees: List[CodeTree]): List[CodeTree] = ???
+  def until(singletonCheck: List[CodeTree] => Boolean,
+            reducer: List[CodeTree] => List[CodeTree])(tree: List[CodeTree]): List[CodeTree] = {
+    if (tree.isEmpty) tree
+    else if (singletonCheck(tree)) tree
+    else until(singleton, combine)(combine(tree))
+  }
 
   /**
    * This function creates a code tree which is optimal to encode the text `chars`.
@@ -137,7 +159,14 @@ trait Huffman extends HuffmanInterface {
    * The parameter `chars` is an arbitrary text. This function extracts the character
    * frequencies from that text and creates a code tree based on them.
    */
-  def createCodeTree(chars: List[Char]): CodeTree = ???
+  def createCodeTree(chars: List[Char]): CodeTree = {
+    if (chars.isEmpty) null
+    else {
+      val list = until(singleton, combine)(makeOrderedLeafList(times(chars)))
+      val tree = list.head
+      tree
+    }
+  }
 
 
   // Part 3: Decoding
@@ -148,7 +177,36 @@ trait Huffman extends HuffmanInterface {
    * This function decodes the bit sequence `bits` using the code tree `tree` and returns
    * the resulting list of characters.
    */
-  def decode(tree: CodeTree, bits: List[Bit]): List[Char] = ???
+  def decode(tree: CodeTree, bits: List[Bit]): List[Char] = {
+    @scala.annotation.tailrec
+    def findLeafChar(tree: CodeTree, bits: List[Bit], accu: List[Char]): (List[Char], List[Bit]) = tree match {
+
+      case l: Leaf => //if tree is a leaf node - the current char needs to be added to accu
+        l.char :: accu //add current char to accu and call with top of tree
+        if (bits.isEmpty) (accu, bits)
+        else (accu, bits.tail) //return newList with char added
+      //if tree is a fork node, recursive call findLeaf with the right or left subtree
+      case f: Fork =>
+        if (bits.tail.nonEmpty) {
+          if (bits.head == 1) findLeafChar(f.left, bits.tail, accu) else findLeafChar(f.right, bits.tail, accu)
+        }
+        else (accu, bits)
+    }
+
+    @scala.annotation.tailrec
+    def decodingAccumulator(tree: CodeTree, bits: List[Bit], chars: List[Char]): List[Char] = {
+      if (bits.nonEmpty) {
+        var newTuple = findLeafChar(tree, bits, List[Char]()) //return value from findLeafChar is (List[Char], List[Bit])
+        decodingAccumulator(tree, newTuple._2, newTuple._1) //recursively call with the the modified values from findLeafChar
+      } else {
+        chars
+      }
+
+    }
+
+    val decodedString = decodingAccumulator(tree, bits, List[Char]())
+    decodedString
+  }
 
   /**
    * A Huffman coding tree for the French language.
@@ -166,7 +224,11 @@ trait Huffman extends HuffmanInterface {
   /**
    * Write a function that returns the decoded secret
    */
-  def decodedSecret: List[Char] = ???
+  def decodedSecret: List[Char] = {
+    val message = decode(frenchCode, secret)
+    println(message)
+    message
+  }
 
 
   // Part 4a: Encoding using Huffman tree
@@ -175,7 +237,29 @@ trait Huffman extends HuffmanInterface {
    * This function encodes `text` using the code tree `tree`
    * into a sequence of bits.
    */
-  def encode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def encode(tree: CodeTree)(text: List[Char]): List[Bit] = {
+    if (text.nonEmpty) {
+      val t = findLeafBit(tree, text, List[Bit]())
+      t
+    } else {
+      List[Bit]()
+    }
+  }
+
+  def findLeafBit(tree: CodeTree, text: List[Char], bits: List[Bit]): List[Bit] = tree match {
+    case f: Fork =>
+      if (chars(f.left).contains(text.head)) { //if left subtree contains target character
+        val newBitList = List[Bit](0)
+        newBitList :: bits
+        findLeafBit(f.left, text, newBitList)
+      } else { //right subtree must contain target character
+        val newBitList = List[Bit](1)
+        newBitList :: bits
+        findLeafBit(f.right, text, newBitList)
+      }
+
+    case l: Leaf => bits //reached a leaf, no more traversing
+  }
 
   // Part 4b: Encoding using code table
 
@@ -185,7 +269,10 @@ trait Huffman extends HuffmanInterface {
    * This function returns the bit sequence that represents the character `char` in
    * the code table `table`.
    */
-  def codeBits(table: CodeTable)(char: Char): List[Bit] = ???
+  def codeBits(table: CodeTable)(char: Char): List[Bit] = {
+    val newTuple: (Char, List[Bit]) = table.find(x => x._1 == char).get
+    newTuple._2
+  }
 
   /**
    * Given a code tree, create a code table which contains, for every character in the
@@ -195,14 +282,37 @@ trait Huffman extends HuffmanInterface {
    * a valid code tree that can be represented as a code table. Using the code tables of the
    * sub-trees, think of how to build the code table for the entire tree.
    */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = {
+    def convertAcc(tree: CodeTree, accuTable: CodeTable): CodeTable = {
+      tree match {
+
+        case f: Fork => {
+          convertAcc(f.left, accuTable)
+          convertAcc(f.right, accuTable)
+        } //end case f
+
+        case l: Leaf => {
+          val newBitList = findLeafBit(l, List[Char](l.char), List[Bit]())
+          val newTuple = (l.char, newBitList)
+          newTuple :: accuTable //add new tuple to accumulator
+        } //end case l
+      } //end pattern matching
+      accuTable
+    }
+
+    val emptyTable: CodeTable = List[(Char, List[Bit])]()
+    convertAcc(tree, emptyTable)
+  }
 
   /**
    * This function takes two code tables and merges them into one. Depending on how you
    * use it in the `convert` method above, this merge method might also do some transformations
    * on the two parameter code tables.
    */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = {
+    a :: b
+    b
+  }
 
   /**
    * This function encodes `text` according to the code tree `tree`.
@@ -210,8 +320,19 @@ trait Huffman extends HuffmanInterface {
    * To speed up the encoding process, it first converts the code tree to a code table
    * and then uses it to perform the actual encoding.
    */
-  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
-}
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = {
+    def quickEncodeAcc(text: List[Char], newTable: CodeTable, accuBitList: List[Bit]): List[Bit] = {
+      if (text.nonEmpty) {
+        val newBitList = codeBits(newTable)(text.head)
+        accuBitList :: newBitList
+        quickEncodeAcc(text.tail, newTable, newBitList)
+      }
+      accuBitList //return accumulator
+    }
 
-object Huffman extends Huffman {
-}
+    val newTable = convert(tree)
+    val newBitList = List[Bit]()
+    quickEncodeAcc(text, newTable, newBitList)
+  }
+
+  object Huffman extends Huffman
